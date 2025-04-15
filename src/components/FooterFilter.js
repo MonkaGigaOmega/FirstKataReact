dffc871fb9b43662b6856ea2cc90ddc5c72ed619
Gosha Shestakov <gosha3103@mail.ru>
2025-04-15 12:17:16 +0300

    сделал смену класса у кнопок

--- src/components/FooterFilter.js
+++ src/components/FooterFilter.js
@@ -1,28 +1,35 @@
+import { useState } from "react";
 export default function FooterFilter({ tasks,setTasks, setFilteredTasks,filteredTasks }) {
+
+  const [selectedButton, setSelectedButton] = useState('All');
+
     const handleShowCompleted = () => {
         setTasks(filteredTasks.filter(task => task.isCompleted));
+        setSelectedButton('Completed');
     };
 
     const handleShowActive = () => {
         setTasks(filteredTasks.filter(task => !task.isCompleted && !task.isEditing));
+        setSelectedButton('Active');
     };
 
     const handleShowAll = () => {
         setTasks(filteredTasks);
+        setSelectedButton('All');
     };
   
     return (
       <ul className="filters">
         <li>
-          <button onClick={handleShowAll} className="selected">
+          <button onClick={handleShowAll}  className={selectedButton === 'All' ? 'selected' : ''}>
             All
           </button>
         </li>
         <li>
-          <button onClick={handleShowActive}>Active</button>
+          <button onClick={handleShowActive} className={selectedButton === 'Active' ? 'selected' : ''}>Active</button>
         </li>
         <li>
-          <button onClick={handleShowCompleted}>Completed</button>
+          <button onClick={handleShowCompleted}  className={selectedButton === 'Completed' ? 'selected' : ''}>Completed</button>
         </li>
       </ul>
     );
